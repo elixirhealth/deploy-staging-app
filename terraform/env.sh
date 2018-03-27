f2647a550e63d8183d10db3707922f210c37b04c
Drausin Wulsin <daedalus2718@gmail.com>
2018-03-26 23:47:24 -0400

    update staging project (#4)

--- terraform/env.sh
+++ terraform/env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # general
-export TF_VAR_gcp_project="elixir-deploy-staging"
+export TF_VAR_gcp_project="elixir-deploy-staging-1"
 export TF_VAR_cluster_name="primary"
 export TF_VAR_region="us-east1"
 export TF_VAR_secrets_app_key_ring_name="deploy-app"
