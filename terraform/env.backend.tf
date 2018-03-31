cf4f569eeb5dcb0d5dfcc7b806ae2f6333c4c7e4
Drausin Wulsin <daedalus2718@gmail.com>
2018-03-31 19:47:33 -0400

    get k8s stuff workign w/ ./deploy-staging-app

--- terraform/env.backend.tf
+++ terraform/env.backend.tf
@@ -4,6 +4,6 @@ terraform {
     prefix      = "primary-app/terraform"
     project     = "elixir-deploy-staging-1"
     region      = "us-east1"
-    credentials = "gcp_credentials.json"
+    credentials = "secret.gcp.creds.json"
   }
 }
