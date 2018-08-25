ad71ad34e5ae82263f763cd483106deb2164d428
Drausin Wulsin <daedalus2718@gmail.com>
2018-08-24 22:39:29 -0400

    remove non-libri service config

--- kubernetes/libri/env.go
+++ kubernetes/libri/env.go
@@ -3,7 +3,7 @@ package main
 // separate env-specific things in own file to reduce complexity of pushing updates to gen.go to
 // deploy-prod-app repo
 const (
-	defaultLibriVersion = "0.3.0"
+	defaultLibriVersion = "0.4.0"
 	defaultCPULimit     = "100m"
 	defaultRAMLimit     = "2G"
 	defaultDiskSize     = 10
